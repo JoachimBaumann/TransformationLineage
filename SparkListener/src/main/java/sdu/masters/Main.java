4b59c2e3632eb25a8fd57e94abcc548ebb63e6f5
Joachim Baumann <joachimrbaumann@gmail.com>
2025-05-19 21:10:06 +0200

    updated howto etc.

--- SparkListener/src/main/java/sdu/masters/Main.java
+++ SparkListener/src/main/java/sdu/masters/Main.java
@@ -4,7 +4,7 @@ package sdu.masters;
 public class Main {
 
     public static void main(String[] args) {
-        KafkaLineageProducer producer = new KafkaLineageProducer("localhost:29092", "LineageEvent");
+        KafkaLineageProducer producer = new KafkaLineageProducer("joachimbaumann.dk:9092", "LineageEvent");
 
         System.out.println("test");
         producer.sendEvent("Hello World");
