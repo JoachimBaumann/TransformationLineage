759ee867ff96d67990636b052d13eba694dc3556
PhilNiel <phillipvincent@hotmail.dk>
2025-05-21 22:19:39 +0200

    third test

--- transformations/wordcount/src/main/java/sdu/masters/WordCount.java
+++ transformations/wordcount/src/main/java/sdu/masters/WordCount.java
@@ -9,6 +9,7 @@ public class WordCount {
         if (args.length < 2) {
             System.err.println("Usage: WordCount <input_file> <output_directory>");
             System.exit(1);
+
         }
 
         SparkSession spark = SparkSession.builder()
